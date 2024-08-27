7c1923a975491932a020ff862153a73dbf75ae64
yuseonjun <seonjun0906@gmail.com>
2024-08-27 11:16:08 +0900

    [Modify] 방명록 페이지 프로젝트 위치 변경

--- src/main/java/com/example/demo/ui/GuestBookController.java
+++ src/main/java/com/example/demo/ui/GuestBookController.java
@@ -1,24 +1,13 @@
 package com.example.demo.ui;
 
-import com.example.demo.application.GuestBookService;
-import com.example.demo.ui.dto.GuestBookCreateRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/guestBook")
+@Controller
 public class GuestBookController {
 
-    private final GuestBookService guestBookService;
-
-    @PostMapping()
-    public ResponseEntity<Long> createGuestBook(@RequestBody final GuestBookCreateRequest guestBookCreateRequest) {
-        final Long guestBookId = guestBookService.save(guestBookCreateRequest);
-        return ResponseEntity.ok(guestBookId);
+    @GetMapping("/create/guestBook")
+    public String guestBookPage() {
+        return "/guestBook/guestBook-save";
     }
 }
---
+++ src/main/java/com/example/demo/ui/api/GuestBookApiController.java
@@ -0,0 +1,24 @@
+package com.example.demo.ui.api;
+
+import com.example.demo.application.GuestBookService;
+import com.example.demo.ui.dto.GuestBookCreateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/guestBook")
+public class GuestBookApiController {
+
+    private final GuestBookService guestBookService;
+
+    @PostMapping()
+    public ResponseEntity<Long> createGuestBook(@RequestBody final GuestBookCreateRequest guestBookCreateRequest) {
+        final Long guestBookId = guestBookService.save(guestBookCreateRequest);
+        return ResponseEntity.ok(guestBookId);
+    }
+}
