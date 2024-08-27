3eb66f928c895266c6ae962bff30398bcedcfe0b
dbrjsdn2015 <dbrjdn2051@naver.com>
2024-08-27 11:53:41 +0900

    index controller / home.html fetch 추가

---
+++ src/main/java/com/example/demo/ui/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "indexWithDetails/home";
+    }
+
+    @GetMapping("/details/{userId}")
+    public String details(@PathVariable String userId){
+        return "indexWithDetails/details/" + userId;
+    }
+    
+}
